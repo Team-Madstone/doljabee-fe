944c32303161cc1c510ae62334962780c9cd53ec
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-18 22:02:48 +0900

    ✨ Feed 뿌려주기

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+import Feed from '../components/Feed';
 import Nav from '../components/Nav';
+import { APP } from '../constances/routes';
+import { getFeeds } from '../services/feed';
+import { TFeed } from '../types/feed';
+import { getErrorState } from '../utils/error';
 
 export default function Home() {
+  const [feeds, setFeeds] = useState<TFeed[]>([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const syncGetFeeds = async () => {
+      try {
+        const response = await getFeeds();
+        setFeeds(response.data);
+      } catch (error) {
+        const state = getErrorState(error);
+        navigate(APP.ERROR, { state });
+      }
+    };
+    syncGetFeeds();
+  }, []);
+
   return (
     <div>
       <Nav />
-      <h1>Home</h1>
+      <div>
+        {feeds &&
+          feeds.map((feed: TFeed) => <Feed key={feed.id} feed={feed} />)}
+      </div>
     </div>
   );
 }
