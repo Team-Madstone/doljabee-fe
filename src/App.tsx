99431bbf955f0a9e3fa9e4fdb45ae008354546df
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-18 22:17:20 +0900

    ✨ feedDetail 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 import './styles/styles.scss';
 import Home from './pages/Home';
 import UploadFeed from './pages/UploadFeed';
+import FeedDetail from './pages/FeedDetail';
+import { APP, FEED } from './constances/routes';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/upload" element={<UploadFeed />}></Route>
+        <Route path={APP.HOME} element={<Home />}></Route>
+        <Route path={FEED.FEED} element={<FeedDetail />}></Route>
+        <Route path={FEED.UPLOAD} element={<UploadFeed />}></Route>
       </Routes>
     </div>
   );
