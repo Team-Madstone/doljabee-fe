27a3ec3073f9c225fddb1524e1f935eb409bb87f
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-18 21:58:24 +0900

    ✨ Feed 추가

---
+++ src/components/Feed.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import { TFeed } from '../types/feed';
+
+type TProps = {
+  feed: TFeed;
+};
+
+export default function Feed({ feed }: TProps) {
+  return (
+    <div>
+      {feed && (
+        <Link to={`/${feed.id}`}>
+          <div className="feedContainer">
+            <span>{feed.id}</span>
+            <span>{feed.createdAt}</span>
+            <h3>{feed.title}</h3>
+            <p>{feed.text}</p>
+            {feed.photo && (
+              <img src={`http://localhost:4000/${feed.photo}`} alt="img" />
+            )}
+            <div>
+              <span>like {feed.likes} </span>
+              <span>comment</span>
+            </div>
+          </div>
+        </Link>
+      )}
+    </div>
+  );
+}
