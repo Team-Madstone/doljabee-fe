2f563a5030cc317700818c4c94982d0fefe8f805
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-18 16:36:15 +0900

    ✨ 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import UploadFeed from './pages/UploadFeed';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/upload" element={<UploadFeed />}></Route>
       </Routes>
     </div>
   );
