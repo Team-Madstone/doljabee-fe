effa80bc96a8d137ff8fce5e3b8bb67e3f03c127
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-18 22:24:41 +0900

    ✨ Error 라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import UploadFeed from './pages/UploadFeed';
 import FeedDetail from './pages/FeedDetail';
 import { APP, FEED } from './constances/routes';
+import Error from './pages/Error';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path={APP.HOME} element={<Home />}></Route>
         <Route path={FEED.FEED} element={<FeedDetail />}></Route>
         <Route path={FEED.UPLOAD} element={<UploadFeed />}></Route>
+        <Route path={APP.ERROR} element={<Error />}></Route>
       </Routes>
     </div>
   );
