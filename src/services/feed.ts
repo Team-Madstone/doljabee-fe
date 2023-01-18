d8577ae84a6addcf6fbb277ab37b29d90fb6297b
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-18 22:30:04 +0900

    ✨ uploadFeed 추가

--- src/services/feed.ts
+++ src/services/feed.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { DOLJABEE_DOMAIN } from '../constances/domain';
-import { TDeleteFeedMutation, TFeed, TGetFeedMutation } from '../types/feed';
+import {
+  TDeleteFeedMutation,
+  TFeed,
+  TGetFeedMutation,
+  TUploadFeedMutation,
+} from '../types/feed';
 
 const axiosInstance = axios.create({
   baseURL: DOLJABEE_DOMAIN,
@@ -10,6 +15,19 @@ export const getFeeds = () => axiosInstance.get<TFeed[]>('/feed');
 
 export const getFeed = ({ id }: TGetFeedMutation) =>
   axiosInstance.get<TFeed>(`/feed/${id}`);
+
+export const uploadFeed = async ({
+  title,
+  text,
+  photo,
+}: TUploadFeedMutation) => {
+  const formData = new FormData();
+  formData.append('title', title);
+  formData.append('text', text);
+  photo && formData.append('photo', photo);
+
+  return axiosInstance.post(`/feed`, formData);
+};
 
 export const deleteFeed = ({ id }: TDeleteFeedMutation) =>
   axiosInstance.delete(`/feed`, {
