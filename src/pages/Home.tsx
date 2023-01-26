430948df2f99fe92a86627a1b3edd4c8d613494c
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-26 16:26:00 +0900

    ✨ 로딩 상태 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router';
 import Feed from '../components/Feed';
+import Loading from '../components/Loading';
 import Nav from '../components/Nav';
 import { APP } from '../constances/routes';
 import { getFeeds } from '../services/feed';
@@ -8,17 +9,21 @@ import { TFeed } from '../types/feed';
 import { getErrorState } from '../utils/error';
 
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   const [feeds, setFeeds] = useState<TFeed[]>([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const syncGetFeeds = async () => {
+      setLoading(true);
       try {
         const response = await getFeeds();
         setFeeds(response.data);
       } catch (error) {
         const state = getErrorState(error);
         navigate(APP.ERROR, { state });
+      } finally {
+        setLoading(false);
       }
     };
     syncGetFeeds();
@@ -27,10 +32,14 @@ export default function Home() {
   return (
     <div>
       <Nav />
-      <div>
-        {feeds &&
-          feeds.map((feed: TFeed) => <Feed key={feed.id} feed={feed} />)}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          {feeds &&
+            feeds.map((feed: TFeed) => <Feed key={feed.id} feed={feed} />)}
+        </div>
+      )}
     </div>
   );
 }
