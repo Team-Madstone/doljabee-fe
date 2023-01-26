86977b271c7061756793c5397ceea0cf40921f91
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-01-26 16:25:56 +0900

    ✨ 로딩 상태 추가

--- src/pages/FeedDetail.tsx
+++ src/pages/FeedDetail.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
+import Loading from '../components/Loading';
 import Nav from '../components/Nav';
 import { APP } from '../constances/routes';
 import { deleteFeed, getFeed } from '../services/feed';
@@ -8,17 +9,21 @@ import { getErrorState } from '../utils/error';
 
 export default function FeedDetail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(false);
   const [feed, setFeed] = useState<TFeed>();
   const navigate = useNavigate();
 
   useEffect(() => {
     const syncGetFeed = async (id: number) => {
+      setLoading(true);
       try {
         const response = await getFeed({ id });
         setFeed(response.data);
       } catch (error) {
         const state = getErrorState(error);
         navigate(APP.ERROR, { state });
+      } finally {
+        setLoading(false);
       }
     };
     syncGetFeed(Number(id));
@@ -40,29 +45,35 @@ export default function FeedDetail() {
   return (
     <div>
       <Nav />
-      {feed && (
-        <div className="feedContainer">
-          <span>{feed.id}</span>
-          <span>{feed.createdAt}</span>
-          <h3>{feed.title}</h3>
-          <p>{feed.text}</p>
-          {feed.photo && (
-            <img
-              src={`http://localhost:4000/${feed.photo}`}
-              style={{ width: '300px' }}
-              alt="img"
-            />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          {feed && (
+            <div className="feedContainer">
+              <span>{feed.id}</span>
+              <span>{feed.createdAt}</span>
+              <h3>{feed.title}</h3>
+              <p>{feed.text}</p>
+              {feed.photo && (
+                <img
+                  src={`http://localhost:4000/${feed.photo}`}
+                  style={{ width: '300px' }}
+                  alt="img"
+                />
+              )}
+              <div>
+                <span>like {feed.likes} </span>
+                <span>comment</span>
+              </div>
+              <div>
+                <Link to={`/${feed.id}/edit`}>
+                  <button>수정하기</button>
+                </Link>
+                <button onClick={() => handleDelete(feed.id)}>삭제하기</button>
+              </div>
+            </div>
           )}
-          <div>
-            <span>like {feed.likes} </span>
-            <span>comment</span>
-          </div>
-          <div>
-            <Link to={`/${feed.id}/edit`}>
-              <button>수정하기</button>
-            </Link>
-            <button onClick={() => handleDelete(feed.id)}>삭제하기</button>
-          </div>
         </div>
       )}
     </div>
