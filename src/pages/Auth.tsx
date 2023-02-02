4ece20c798ef6274b5cbd2aa8c4755556f6f9942
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-02 18:37:04 +0900

    ✨ 토큰 인증 로직 추가

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,31 @@
+import qs from 'qs';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { APP } from '../constances/routes';
+import { signToken } from '../services/user';
+
 export default function Auth() {
+  const query = qs.parse(window.location.search, {
+    ignoreQueryPrefix: true,
+  });
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        if (query.token) {
+          await signToken(query.token as string);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    verifyToken();
+  }, [query]);
   return (
     <div>
-      <p>Auth</p>
+      <Link to={APP.LOGIN}>
+        <button>로그인 하기</button>
+      </Link>
     </div>
   );
 }
