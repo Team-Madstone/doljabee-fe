ecb0ff7f1089ac12d198208e12215eee43abfeba
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-02 18:37:38 +0900

    🥅 토큰 인증 에러 처리

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,13 +1,15 @@
-import qs from 'qs';
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { APP } from '../constances/routes';
+import qs from 'qs';
 import { signToken } from '../services/user';
+import { AxiosError } from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function Auth() {
   const query = qs.parse(window.location.search, {
     ignoreQueryPrefix: true,
   });
+  const [authErr, setAuthErr] = useState<string>();
 
   useEffect(() => {
     const verifyToken = async () => {
@@ -16,13 +18,21 @@ export default function Auth() {
           await signToken(query.token as string);
         }
       } catch (error) {
-        console.log(error);
+        if (error instanceof AxiosError) {
+          const errorMsg: string = error.response?.data?.message;
+          if (errorMsg) {
+            setAuthErr(errorMsg);
+          }
+        }
       }
     };
     verifyToken();
   }, [query]);
+
   return (
     <div>
+      {authErr ? <p>{authErr}</p> : <p>인증이 완료되었습니다.</p>}
+
       <Link to={APP.LOGIN}>
         <button>로그인 하기</button>
       </Link>
