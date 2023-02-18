7a6396c7bd91debdad0f9f778ef90fbb8a0f22d6
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-19 02:44:34 +0900

    💄 로그인 UI 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,30 @@
+import { Link } from 'react-router-dom';
 import Nav from '../components/Nav';
+import { APP } from '../constances/routes';
 
 export default function Login() {
   return (
     <div>
       <Nav />
       <div>Login</div>
+      <form method="POST">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">로그인</button>
+        </div>
+        <div>
+          <Link to={APP.SIGNUP}>
+            <button>회원가입</button>
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
