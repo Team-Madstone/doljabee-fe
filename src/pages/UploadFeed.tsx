f00a8603b186d6a28d109a5dfc3e0997e4d6a443
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-19 23:18:28 +0900

    ♻️ uploadFeedMutation 대체

--- src/pages/UploadFeed.tsx
+++ src/pages/UploadFeed.tsx
@@ -1,20 +1,36 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Nav from '../components/Nav';
 import { APP } from '../constances/routes';
 import { uploadFeed } from '../services/feed';
 import { TFormValue } from '../types/feed';
-import { getErrorState } from '../utils/error';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { UserContext } from '../context/UserContext';
+import { useMutation } from 'react-query';
+import Loading from '../components/Loading';
+import { isAxiosError } from 'axios';
 
 export default function UploadFeed() {
+  const { user, isLoading: userLoading } = useContext(UserContext);
+  const [preview, setPreview] = useState<string>();
+  const navigate = useNavigate();
+
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<TFormValue>();
-  const [preview, setPreview] = useState<string>();
-  const navigate = useNavigate();
+
+  const {
+    mutate: uploadFeedMutation,
+    isLoading,
+    isError,
+    error,
+  } = useMutation(uploadFeed, {
+    onSuccess: () => {
+      navigate(APP.HOME);
+    },
+  });
 
   const handlePhotoFileChange = async (event: any) => {
     const file = event.target.files[0];
@@ -26,14 +42,21 @@ export default function UploadFeed() {
     text,
     photoFile,
   }: TFormValue) => {
-    try {
-      uploadFeed({ title, text, photoFile: photoFile?.[0] });
-      navigate(APP.HOME);
-    } catch (error) {
-      const state = getErrorState(error);
-      navigate(APP.ERROR, { state });
-    }
+    uploadFeedMutation({ title, text, photoFile: photoFile?.[0] });
   };
+
+  useEffect(() => {
+    if (!userLoading && !user) {
+      navigate(APP.LOGIN);
+    } else if (user && !user.verifyEmail) {
+      alert('이메일 인증 후 이용할 수 있습니다.');
+      navigate(APP.HOME);
+    }
+  }, [user, userLoading, navigate]);
+
+  if (userLoading) {
+    return <Loading />;
+  }
 
   return (
     <div>
@@ -91,8 +114,11 @@ export default function UploadFeed() {
             <img style={{ width: '300px' }} src={preview} alt="preview"></img>
           )}
         </div>
-        <button type="submit" className="uploadBtn">
-          올리기
+        {isError && isAxiosError(error) && (
+          <p>{error.response?.data.message}</p>
+        )}
+        <button type="submit" className="uploadBtn" disabled={isLoading}>
+          {isLoading ? '업로드 중' : '올리기'}
         </button>
       </form>
     </div>
