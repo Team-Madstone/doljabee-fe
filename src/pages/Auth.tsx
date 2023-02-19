356507d08243c857b2134fc8733194c1fb3e562d
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-19 16:49:26 +0900

    ♻️ authQuery 대체
    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,37 +2,33 @@ import { Link } from 'react-router-dom';
 import { APP } from '../constances/routes';
 import qs from 'qs';
 import { verifyEmail } from '../services/user';
-import { AxiosError } from 'axios';
-import { useEffect, useState } from 'react';
+import { isAxiosError } from 'axios';
+import { useQuery } from 'react-query';
+import Loading from '../components/Loading';
+import NotFound from './NotFound';
+import Nav from '../components/Nav';
 
 export default function Auth() {
   const query = qs.parse(window.location.search, {
     ignoreQueryPrefix: true,
   });
-  const [authErr, setAuthErr] = useState<string>();
 
-  useEffect(() => {
-    const verifyToken = async () => {
-      try {
-        if (query.token) {
-          await verifyEmail(query.token as string);
-        }
-      } catch (error) {
-        if (error instanceof AxiosError) {
-          const errorMsg: string = error.response?.data?.message;
-          if (errorMsg) {
-            setAuthErr(errorMsg);
-          }
-        }
-      }
-    };
-    verifyToken();
-  }, [query]);
+  const { isLoading, isError, error } = useQuery('verifyEmail', () =>
+    verifyEmail(query.token as string)
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError && isAxiosError(error)) {
+    return <NotFound />;
+  }
 
   return (
     <div>
-      {authErr ? <p>{authErr}</p> : <p>인증이 완료되었습니다.</p>}
-
+      <Nav />
+      <p>인증이 완료되었습니다.</p>
       <Link to={APP.LOGIN}>
         <button>로그인 하기</button>
       </Link>
