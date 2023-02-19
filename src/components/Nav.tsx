41ff7e62c75f958ea28463c54f498fca8c01b0eb
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-19 16:06:21 +0900

    💄 네비 UI 추가 및 스타일링

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,42 @@
-import { NavLink } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 export default function Nav() {
+  const { user } = useContext(UserContext);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div>
       <nav className="nav">
         <div className="navMenu">
-          <NavLink to="/">Home</NavLink>
+          <NavLink to="/">홈</NavLink>
         </div>
-        <div className="navMenu">
-          <NavLink to="/upload">Upload</NavLink>
-        </div>
-        <div className="navMenu">
-          <NavLink to="/login">Login</NavLink>
-        </div>
+        {user ? (
+          <div className="navMenu">
+            <button
+              type="button"
+              className="myBtn"
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
+            >
+              마이페이지
+            </button>
+            {isMenuOpen && (
+              <div className="menuWrapper">
+                <div className="menu">
+                  <Link to="/my-profile">내 프로필</Link>
+                  <Link to="/upload">업로드</Link>
+                  <button type="button">로그아웃</button>
+                </div>
+              </div>
+            )}
+          </div>
+        ) : (
+          <div className="navMenu">
+            <NavLink to="/login">Login</NavLink>
+          </div>
+        )}
       </nav>
     </div>
   );
