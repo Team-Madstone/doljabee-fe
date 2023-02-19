4d6ccb04f1c566e67525b9d4050d1cd31d9e9d88
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-19 23:00:26 +0900

    ♻️ getFeedQuery 대체

--- src/pages/FeedDetail.tsx
+++ src/pages/FeedDetail.tsx
@@ -1,81 +1,73 @@
-import { useEffect, useState } from 'react';
+import { isAxiosError } from 'axios';
+import { useContext } from 'react';
+import { useQuery } from 'react-query';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import Loading from '../components/Loading';
 import Nav from '../components/Nav';
-import { APP } from '../constances/routes';
+import { UserContext } from '../context/UserContext';
 import { deleteFeed, getFeed } from '../services/feed';
-import { TFeed } from '../types/feed';
-import { getErrorState } from '../utils/error';
+import NotFound from './NotFound';
 
 export default function FeedDetail() {
+  const { user, isLoading: userLoading } = useContext(UserContext);
   const params = useParams();
-  const _id = params.id;
-  const [loading, setLoading] = useState(false);
-  const [feed, setFeed] = useState<TFeed>();
+  const _id = params.id as string;
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const syncGetFeed = async (_id: string) => {
-      setLoading(true);
-      try {
-        const response = await getFeed({ _id });
-        setFeed(response.data);
-      } catch (error) {
-        const state = getErrorState(error);
-        navigate(APP.ERROR, { state });
-      } finally {
-        setLoading(false);
-      }
-    };
-    _id && syncGetFeed(_id);
-  }, []);
+  const { isLoading, isError, data, error } = useQuery('getFeed', () =>
+    getFeed({ _id })
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError && isAxiosError(error)) {
+    return <NotFound />;
+  }
 
   const handleDelete = async (_id: string) => {
-    deleteFeed({ _id })
-      .then((response) => {
-        if (response.status === 200) {
-          return navigate('/');
-        }
-      })
-      .catch((error) => {
-        const state = getErrorState(error);
-        navigate(APP.ERROR, { state });
-      });
+    try {
+      deleteFeed({ _id });
+      navigate('/');
+    } catch (error: any) {
+      alert(error.response.data.message);
+    }
   };
 
   return (
     <div>
       <Nav />
-      {loading ? (
-        <Loading />
-      ) : (
-        <div>
-          {feed && (
-            <div className="feedContainer">
-              <span>{feed.createdAt}</span>
-              <h3>{feed.title}</h3>
-              <p>{feed.text}</p>
-              {feed.photo && (
-                <img
-                  src={`http://localhost:4000/${feed.photo}`}
-                  style={{ width: '300px' }}
-                  alt="img"
-                />
-              )}
+      <div>
+        {data && (
+          <div className="feedContainer">
+            <span>{data.data.createdAt}</span>
+            <h3>{data.data.title}</h3>
+            <p>{data.data.text}</p>
+            {data.data.photo && (
+              <img
+                src={`http://localhost:4000/${data.data.photo}`}
+                style={{ width: '300px' }}
+                alt="img"
+              />
+            )}
+            <div>
+              <span>like {data.data.likes} </span>
+              <span>comment</span>
+            </div>
+            {user && !userLoading && (
               <div>
-                <span>like {feed.likes} </span>
-                <span>comment</span>
-              </div>
-              <div>
-                <Link to={`/${feed._id}/edit`}>
+                <Link to={`/${data.data._id}/edit`}>
                   <button>수정하기</button>
                 </Link>
-                <button onClick={() => handleDelete(feed._id)}>삭제하기</button>
+                <button onClick={() => handleDelete(data.data._id)}>
+                  삭제하기
+                </button>
               </div>
-            </div>
-          )}
-        </div>
-      )}
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
