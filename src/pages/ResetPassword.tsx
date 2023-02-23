dcab9c78ae81ee797acfcf2b4591078c70ccace3
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-02-23 22:04:29 +0900

    👽 resetPasswordMutation 추가

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,10 +1,14 @@
+import { isAxiosError } from 'axios';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { useLocation } from 'react-router';
+import { useMutation } from 'react-query';
+import { useLocation, useNavigate } from 'react-router';
 import Nav from '../components/Nav';
+import { APP } from '../constances/routes';
 import { resetPassword } from '../services/user';
 import { TResetPassword } from '../types/user';
 
 export default function ResetPassword() {
+  const navigate = useNavigate();
   const location = useLocation();
   const email = location.search.split('=')[1].split('&')[0];
 
@@ -14,11 +18,23 @@ export default function ResetPassword() {
     handleSubmit,
   } = useForm<TResetPassword>();
 
+  const {
+    mutate: resetPasswordMutation,
+    isLoading,
+    isError,
+    error,
+  } = useMutation(resetPassword, {
+    onSuccess: () => {
+      alert('비밀번호가 변경되었습니다.');
+      navigate(APP.HOME);
+    },
+  });
+
   const onValid: SubmitHandler<TResetPassword> = async ({
     newPassword,
     newPasswordConfirmation,
   }) => {
-    resetPassword({
+    resetPasswordMutation({
       email,
       newPassword,
       newPasswordConfirmation,
@@ -74,7 +90,12 @@ export default function ResetPassword() {
             </p>
           )}
         </div>
-        <button type="submit">변경하기</button>
+        {isError && isAxiosError(error) && (
+          <p>{error.response?.data.message}</p>
+        )}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? '변경중' : '변경하기'}
+        </button>
       </form>
     </div>
   );
