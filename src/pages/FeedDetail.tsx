921804d14e57479c67837a091e08c7546dc44549
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-03-04 22:48:34 +0900

    👽 getFeedQuery 수정

--- src/pages/FeedDetail.tsx
+++ src/pages/FeedDetail.tsx
@@ -1,5 +1,5 @@
 import { isAxiosError } from 'axios';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import Loading from '../components/Loading';
@@ -14,17 +14,15 @@ export default function FeedDetail() {
   const _id = params.id as string;
   const navigate = useNavigate();
 
-  const { isLoading, isError, data, error } = useQuery('getFeed', () =>
+  const { isLoading, isError, data, error } = useQuery(['getFeed', _id], () =>
     getFeed({ _id })
   );
 
-  if (isLoading) {
-    return <Loading />;
-  }
-
-  if (isError && isAxiosError(error)) {
-    return <NotFound />;
-  }
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+  }, [data]);
 
   const handleDelete = async (_id: string) => {
     try {
@@ -35,38 +33,48 @@ export default function FeedDetail() {
     }
   };
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError && isAxiosError(error)) {
+    return <NotFound />;
+  }
+
+  if (!isLoading && !data?.data) {
+    return <NotFound />;
+  }
+
+  const feed = data.data;
+
   return (
     <div>
       <Nav />
       <div>
-        {data && (
-          <div className="feedContainer">
-            <span>{data.data.createdAt}</span>
-            <h3>{data.data.title}</h3>
-            <p>{data.data.text}</p>
-            {data.data.photo && (
-              <img
-                src={`http://localhost:4000/${data.data.photo}`}
-                style={{ width: '300px' }}
-                alt="img"
-              />
-            )}
-            <div>
-              <span>like {data.data.likes} </span>
-              <span>comment</span>
-            </div>
-            {user && !userLoading && (
-              <div>
-                <Link to={`/${data.data._id}/edit`}>
-                  <button>수정하기</button>
-                </Link>
-                <button onClick={() => handleDelete(data.data._id)}>
-                  삭제하기
-                </button>
-              </div>
-            )}
+        <div className="feedContainer">
+          <span>{feed.createdAt}</span>
+          <h3>{feed.title}</h3>
+          <p>{feed.text}</p>
+          {feed.photo && (
+            <img
+              src={`http://localhost:4000/${feed.photo}`}
+              style={{ width: '300px' }}
+              alt="img"
+            />
+          )}
+          <div>
+            <span>like {feed.likes ? feed.likes.length : 0} </span>
+            <span>comment</span>
           </div>
-        )}
+          {user && !userLoading && (
+            <div>
+              <Link to={`/${feed._id}/edit`}>
+                <button>수정하기</button>
+              </Link>
+              <button onClick={() => handleDelete(feed._id)}>삭제하기</button>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
