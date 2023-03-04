87101ae9d099788b1737bff8fc874e0161287224
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-03-04 23:44:02 +0900

    👽 createCommentMutation 추가

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,9 +1,12 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation, useQueryClient } from 'react-query';
 import { useParams } from 'react-router';
 import { createComment } from '../services/comment';
 import { TCommentForm } from '../types/comment';
+import { TError } from '../types/feed';
 
 export default function CommentForm() {
+  const queryClient = useQueryClient();
   const params = useParams();
   const _id = params.id as string;
 
@@ -11,13 +14,26 @@ export default function CommentForm() {
     register: createCommentRegister,
     formState: { errors: createCommentErrors },
     handleSubmit: createCommentHandleSubmit,
+    setValue: createCommentSetValue,
   } = useForm<TCommentForm>();
+
+  const { mutate: createCommentMutation } = useMutation(createComment, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['getFeed', _id]);
+      createCommentSetValue('text', '');
+    },
+    onError: (error: TError) => {
+      if (error.response.status === 401) {
+        alert('로그인 후 이용해주세요.');
+      }
+    },
+  });
 
   const onValid: SubmitHandler<TCommentForm> = ({ text }) => {
     if (!text.trim()) {
       return;
     }
-    createComment({ _id, text });
+    createCommentMutation({ _id, text });
   };
 
   return (
