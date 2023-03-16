d11bea81d306b2bdb954cbefc8949b1bb56f50de
dev-jihye <59763645+dev-jihye@users.noreply.github.com>
2023-03-16 16:37:33 +0900

    💄 notice 스타일 수정

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,18 +1,21 @@
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { APP } from '../constances/routes';
+import styles from '../styles/notFound.module.scss';
 
 export default function NotFound() {
   return (
     <div>
       <Layout>
-        <div>404 Not Found</div>
-
-        <p>존재하지 않는 페이지입니다.</p>
-
-        <Link to={APP.HOME}>
-          <button>홈으로 가기</button>
-        </Link>
+        <div className={styles.div}>
+          <h2 className={styles.title}>404 Not Found</h2>
+          <p className={styles.p}>존재하지 않는 페이지입니다.</p>
+          <div className={styles.btnWrapper}>
+            <Link to={APP.HOME}>
+              <button className={styles.btn}>홈으로 가기</button>
+            </Link>
+          </div>
+        </div>
       </Layout>
     </div>
   );
--- src/pages/Notice.tsx
+++ src/pages/Notice.tsx
@@ -1,19 +1,27 @@
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
 import { APP } from '../constances/routes';
+import styles from '../styles/notice.module.scss';
 
 export default function Notice() {
   return (
     <div>
       <Layout>
-        <p>가입한 이메일로 인증 시 원활한 서비스를 이용할 수 있습니다.</p>
-        <p>이메일 인증은 1시간 뒤 만료됩니다.</p>
-        <Link to={APP.LOGIN}>
-          <button>로그인 하기</button>
-        </Link>
-        <Link to={APP.HOME}>
-          <button>홈으로 가기</button>
-        </Link>
+        <div className={styles.div}>
+          <h2 className={styles.title}>회원가입 완료</h2>
+          <p className={styles.p}>
+            가입한 이메일로 인증 시 원활한 서비스를 이용할 수 있습니다. <br />
+            이메일 인증은 1시간 뒤 만료됩니다.
+          </p>
+          <div className={styles.btnWrapper}>
+            <Link to={APP.LOGIN}>
+              <button className={styles.btn}>로그인 하기</button>
+            </Link>
+            <Link to={APP.HOME}>
+              <button className={styles.btn}>홈으로 가기</button>
+            </Link>
+          </div>
+        </div>
       </Layout>
     </div>
   );
